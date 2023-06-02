78c3eca89b8ba639dbcee822d735cd92e5ba19d3
vasya <shyna0890@gmail.com>
2023-06-02 11:33:53 +0300

    Added search controller

---
+++ app/Http/Requests/SearchRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * Class SearchRequest.
+ */
+class SearchRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'query' => ['nullable', 'string', 'max:255'],
+            'page'  => ['nullable', 'numeric'],
+        ];
+    }
+}
